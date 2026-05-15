3aebabab859252d3591829457f5007a88ae78311
Andre <andre.babirian2004@gmail.com>
2026-05-15 20:43:35 +0200

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,20 @@
+import './index.css'
+
+function App() {
+  return (
+    <>
+      <section id="center">
+        <div className="container">
+          <div className="left">
+            <h1>UI component library website</h1>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptas
+            </p>
+          </div>
+        </div>
+      </section>
+    </>
+  )
+}
+
+export default App
